073954d5392d988ec9b03cac5ae078f53a89d313
wdwiramadhan <wahyu_dr71@yahoo.com>
2020-05-10 01:47:39 +0700

    update passport facebook

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,9 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy;
 const bcrypt = require('bcrypt')
 const User = require('../models/User');
+require("dotenv").config();
 
 
 const initialize = () => {
@@ -11,10 +13,13 @@ const initialize = () => {
       if(!user){
         return done(null, false, { message: 'No user with that email' })
       }
-      if(await bcrypt.compareSync(password, user.password)){
-        return done(null, user)
+      if(user.account.type != 'local'){
+        return done(null, false, { message: 'No user with that email' })
+      }
+      if(await bcrypt.compareSync(password, user.account.password)){
+        done(null, user)
       }else{
-        return done(null, false, { message: 'Password incorrect' })
+        done(null, false, { message: 'Password incorrect' })
       }
 
     }catch(err){
@@ -22,13 +27,42 @@ const initialize = () => {
     }
   }
   passport.use(new LocalStrategy({ usernameField: 'email' },authenticate))
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "/login/facebook/callback",
+    profileFields: ['id', 'emails', 'name']
+  },
+  async(accessToken, refreshToken, profile, done) => {
+    try{
+      var user = await User.findOne({email:profile.emails[0].value});
+      if(user == null){
+        const data = {
+          name : profile.name.familyName,
+          email : profile.emails[0].value,
+          account: {
+            type:profile.provider,
+            uid:profile.id
+          }
+        }
+        user = await User.create(data);
+        return done(null, user);
+      }
+      if(user){
+        return done(null, user);
+      }
+    }catch(err){
+      return done(err);
+    }
+  }
+  ));
   passport.serializeUser(function(user, done) {
     done(null, user.email);
   });
   
   passport.deserializeUser(function(email, done) {
     User.findOne({email:email}, function(err, user) {
-      done(err, user);
+      done(null, user);
     });
   });
 }
